e13948764464107954ae0091ac74d632e9a2eb4d
MarianoIvasiuta <luis.ivasiuta@fceqyn.unam.edu.ar>
2023-09-05 16:51:46 -0300

    update completar historia clinica

--- resources/views/paciente/historia-clinica/create.blade.php
+++ resources/views/paciente/historia-clinica/create.blade.php
@@ -8,23 +8,24 @@
 
 @section('content')
 
+
     <div class="container mt-4">
         <div class="row">
-            <div class="col-md-8">
-                <div class="card">
+            <div class="col-md-12">
+                <div class="card card-dark">
                     <div class="card-header">
                         Datos Personales
                     </div>
                     <div class="card-body">
                         <form class="row g-3" action="{{route('historia-clinica.store')}}" method="POST">
+                           @csrf
                             <div class="col-md-6">
-
                                 <label for="dni" class="form-label">DNI</label>
                                 <input type="text" class="form-control" id="dni">
                             </div>
                             <div class="col-md-6">
-                            <label for="telefono" class="form-label">Teléfono</label>
-                            <input type="text" class="form-control" id="telefono">
+                                <label for="telefono" class="form-label">Teléfono</label>
+                                <input type="text" class="form-control" id="telefono">
                             </div>
 
                             <div class="col-md-4">
@@ -47,21 +48,82 @@
                             </div>
 
 
+                            <span>Seleccione los días que tiene disponibles:</span>
+                            <div class="col-md-2">
+                                <div class="icheck-primary">
+                                    <input value="Lunes" type="checkbox" id="diasFijos-lunes" />
+                                    <label for="diasFijos-lunes">Lunes</label>
+                                </div>
+                                <div class="icheck-primary">
+                                    <input value="Martes" type="checkbox" id="diasFijos-martes" />
+                                    <label for="diasFijos-martes">Martes</label>
+                                </div>
+                            </div>
+
+                            <div class="col-md-2">
+                                <div class="icheck-primary">
+                                    <input value="Miercoles" type="checkbox" id="diasFijos-miercoles" />
+                                    <label for="diasFijos-miercoles">Miércoles</label>
+                                </div>
+                                <div class="icheck-primary">
+                                    <input value="Jueves" type="checkbox" id="diasFijos-jueves" />
+                                    <label for="diasFijos-jueves">Jueves</label>
+                                </div>
+                            </div>
+                            
+                            <span>Seleccione las horas disponibles:</span>
+                            <select class="selectpicker" multiple title="Seleccione las horas de la mañana disponibles..." data-style="btn-success" data-width="fit" data-live-search="true" data-size="5">
+                                <option>8:00</option>
+                                <option>8:15</option>
+                                <option>8:30</option>
+                                <option>8:45</option>
+                                <option>9:00</option>
+                                <option>9:15</option>
+                                <option>9:30</option>
+                                <option>9:45</option>
+                                <option>10:00</option>
+                                <option>10:15</option>
+                                <option>10:30</option>
+                                <option>10:45</option>
+                                <option>11:00</option>
+                                <option>11:15</option>
+                                <option>11:30</option>
+                                <option>11:45</option>
+                                <option data-divider="true" value="12:00">12:00</option>
+                            </select>
+                            <select class="selectpicker" data-style="btn-success" multiple title="Seleccione las horas de la tarde disponibles..." data-width="fit" data-size="5" data-live-search="true">
+                                <option>16:30</option>
+                                <option>16:45</option>
+                                <option>17:00</option>
+                                <option>17:15</option>
+                                <option>17:30</option>
+                                <option>17:45</option>
+                                <option>18:00</option>
+                                <option>18:15</option>
+                                <option>18:30</option>
+                                <option>18:45</option>
+                                <option>19:00</option>
+                                <option>19:15</option>
+                                <option>19:30</option>
+                            </select>
+
                             <div class="col-12">
-                                <button type="submit" class="btn btn-primary">Sign in</button>
+                                <button type="submit" class="btn btn-primary">Guardar</button>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="card">
+
+            <div class="col-md-12">
+                <div class="card card card-dark">
                     <div class="card-header">
                         Días y Horas Fijos disponibles
                     </div>
                     <div class="card-body">
-                        <form>
-                            <!-- Contenido del segundo formulario -->
+                        <form class="row g-3" action="{{route('historia-clinica.store')}}" method="POST">
+
+
                         </form>
                     </div>
                 </div>
@@ -84,11 +146,13 @@
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/dist/css/bootstrap-select.min.css" rel="stylesheet">
 @stop
 
 @section('js')
     <script> console.log('Hi!'); </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/js/bootstrap-select.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
     <script>
