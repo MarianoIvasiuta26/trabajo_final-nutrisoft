17af6aa665cacfe1926feb64d5f1e7948a398647
MarianoIvasiuta <luis.ivasiuta@fceqyn.unam.edu.ar>
2023-09-10 20:21:12 -0300

    update datos médicos de form completar historia clínica

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,15 +11,23 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 bg-white border-b border-gray-200">
-                ¡Bienvenido! Has iniciado sesión correctamente.
 
-                @if(auth()->user()->tipo_usuario === 'Paciente' && !app('App\Http\Controllers\PacienteController')->hasCompletedHistory())
-                    <p>
-                        Parece que aún no has completado tu Historia Clínica.
-                        <a href="{{ route('historia-clinica.create') }}" class="text-blue-500 hover:underline">Completar mi Historia Clínica</a>
-                    </p>
-                @endif
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>¡Bienvenido, {{auth()->user()->name}}!</strong> Has iniciado sesión correctamente.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </div>
+
             </div>
+
+            @if(auth()->user()->tipo_usuario === 'Paciente' && !app('App\Http\Controllers\PacienteController')->hasCompletedHistory())
+
+                <div class="alert alert-warning" role="alert">
+                    Parece que aún no has completado tu Historia Clínica. <br>
+                    Haga click en el siguiente enlace para completar su historia clínica:
+                    <a href="{{ route('historia-clinica.create') }}" class="alert-link">Completar mi Historia Clínica</a>
+                </div>
+
+            @endif
         </div>
     </div>
 </div>
--- resources/views/paciente/historia-clinica/create.blade.php
+++ resources/views/paciente/historia-clinica/create.blade.php
@@ -3,14 +3,14 @@
 @section('title', 'Mi Historia Clínica')
 
 @section('content_header')
-    <h1>Completar Historia Clínica</h1>
+    <h1 style="text-align:center;">Completar Historia Clínica</h1>
 @stop
 
 @section('content')
 
-
     <div class="container mt-4">
         <div class="row">
+            <!--Form datos personales-->
             <div class="col-md-12">
                 <div class="card card-dark">
                     <div class="card-header">
@@ -63,6 +63,7 @@
                 </div>
             </div>
 
+            <!--Form días y horas disponibles para adelantamiento de turnos -->
             <div class="col-md-12">
                 <div class="card card-dark">
                     <div class="card-header">
@@ -83,7 +84,7 @@
                                             @if ($dia->id == $horario->dia_atencion_id)
                                                 <div class="col-md-2">
                                                     <div class="icheck-primary">
-                                                        <input value="{{$dia->dia}}" type="checkbox" id="diasFijos-{{$dia->dia}}" />
+                                                        <input value="{{$dia->dia}}" type="checkbox" id="diasFijos-{{$dia->dia}}" name="diasFijos[]"/>
                                                         <label for="diasFijos-{{$dia->dia}}">{{$dia->dia}}</label>
                                                     </div>
                                                 </div>
@@ -95,7 +96,7 @@
                                 <div class="col-md-6">
                                     <h5>Seleccione las horas disponibles:</h5>
                                     <div class="row">
-                                        <select class="selectpicker" multiple title="Seleccione las horas de la mañana disponibles..." data-style="btn-success" data-width="fit" data-live-search="true" data-size="5">
+                                        <select name="horasFijas[]" class="selectpicker" multiple title="Seleccione las horas de la mañana disponibles..." data-style="btn-success" data-width="fit" data-live-search="true" data-size="5">
                                             <option>8:00</option>
                                             <option>8:15</option>
                                             <option>8:30</option>
@@ -149,15 +150,17 @@
                     </div>
                 </div>
             </div>
+
+            <!--Form datos físicos-->
             <div class="col-md-12">
                 <div class="card card-dark">
                     <div class="card-header">
-                        <button class="btn btn-link float-right" onclick="toggleCard('datosMedicos')">
+                        <button class="btn btn-link float-right" onclick="toggleCard('datosFisicos')">
                             <i class="fa fa-plus"></i>
                         </button>
-                        <h5>Datos médicos</h5>
+                        <h5>Datos Físicos</h5>
                     </div>
-                    <div id="datosMedicos" class="card-body" style="display: none;">
+                    <div id="datosFisicos" class="card-body" style="display: none;">
                         <form action="{{route('historia-clinica.store')}}" method="POST">
                             @csrf
 
@@ -231,6 +234,161 @@
                 </div>
             </div>
 
+            <!--Form datos médicos-->
+            <div class="col-md-12">
+                <div class="card card-dark">
+                    <div class="card-header">
+                        <button class="btn btn-link float-right" onclick="toggleCard('datosMedicos')">
+                            <i class="fa fa-plus"></i>
+                        </button>
+                        <h5>Datos Médicos</h5>
+                    </div>
+                    <div id="datosMedicos" class="card-body" style="display: none;">
+                        <form action="{{route('historia-clinica.store')}}" method="POST">
+                            @csrf
+
+
+                            <div class="row">
+                                <h5>Anamnesis Alimentaria</h5>
+                                <div class="col-md-6">
+                                    <label for="gustos" class="form-label">Seleccione sus alimentos preferidos</label>
+                                    <select name="alimentos_gustos[]" class="form-select" id="gustos" data-placeholder="Alimentos preferidos..." multiple>
+
+                                    </select>
+                                </div>
+                                <div class="col-md-6">
+                                    <label for="no_gustos" class="form-label">Seleccione los alimentos que no le guste</label>
+                                    <select name="alimentos_no_gustos[]" class="form-select" id="no_gustos" data-placeholder="Alimentos que no guste..." multiple>
+
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="row mt-3">
+                                <h5>Alergias</h5>
+                                <div class="col-md-6">
+                                    <label for="alergias" class="form-label">Seleccione las alergias que posee</label>
+                                    <select name="alergias[]" class="form-select" id="alergias" data-placeholder="Alergias..." multiple>
+                                        <option value="">Ninguna</option>
+                                        <optgroup label="Alergia a Frutos secos">
+                                            <option value="">Nueces</option>
+                                            <option value="">Almendras</option>
+                                            <option value="">Avellanas</option>
+                                            <option value="">Pistachos</option>
+                                            <option value="">Anacardos</option>
+
+                                        </optgroup>
+
+                                        <optgroup label="Alergia al Pescado">
+                                            <option value="">Salmón</option>
+                                            <option value="">Atún</option>
+                                            <option value="">Trucha</option>
+                                            <option value="">Sardinas</option>
+                                            <option value="">Bacalao</option>
+                                            <option value="">Merluza</option>
+                                        </optgroup>
+
+                                        <optgroup label="Alergia a Mariscos">
+                                            <option value="">Camarón</option>
+                                            <option value="">Langosta</option>
+                                            <option value="">Cangrejo</option>
+                                            <option value="">Ostras</option>
+                                            <option value="">Mejillones</option>
+                                        </optgroup>
+
+                                        <optgroup label="Otras alergias">
+                                            <option value="">Maní</option>
+                                            <option value="">Huevo y derivados</option>
+                                            <option value="">Leche y derivados</option>
+                                            <option value="">Soja y derivados</option>
+                                            <option value="">Trigo y gluten</option>
+                                            <option value="">Apio</option>
+                                            <option value="">Sésamo</option>
+                                            <option value="">Sulfitos</option>
+                                            <option value="">Lupino</option>
+                                            <option value="">Moluscos</option>
+                                        </optgroup>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="row mt-3">
+                                <h5>Cirugías</h5>
+                                <div class="col-md-6">
+                                    <label for="cirugias" class="form-label">Seleccione las cirugías que se realizó</label>
+                                    <select name="cirugias[]" class="form-select" id="cirugias" data-placeholder="Cirugías..." multiple>
+                                        <option value="">Ninguna</option>
+                                        <optgroup label="Cirugia reciente">
+                                            <option value="">Cirugía oral</option>
+                                            <option value="">Apendicetomía</option>
+                                        </optgroup>
+
+                                        <optgroup label="Cirugía Gastrointestinal">
+                                            <option value="">Resección intestinal</option>
+                                            <option value="">cirugía de pérdida de peso</option>
+                                        </optgroup>
+
+                                        <optgroup label="Cirugía Cardíaca o Vascular">
+                                            <option value="">Bypass de arteria coronaria</option>
+                                            <option value="">Cirugía de válvula cardíaca</option>
+                                            <option value="">Cirugía de aneurisma aórtico</option>
+                                            <option value="">Cirugía de arteria carótida</option>
+                                        </optgroup>
+
+                                        <optgroup label="Cirugía Bariátrica">
+                                            <option value="">Bypass gástrico</option>
+                                            <option value="">Banda gástrica</option>
+                                        </optgroup>
+
+                                        <optgroup label="Cirugía de Órganos Vitales">
+                                            <option value="">Cirugía de Hígado</option>
+                                            <option value="">Cirugía de Páncreas</option>
+                                            <option value="">Cirugía de Riñón</option>
+                                            <option value="">Cirugía de Pulmón</option>
+                                            <option value="">Cirugía de Vejiga</option>
+                                            <option value="">Cirugía de Próstata</option>
+                                            <option value="">Cirugía de Vesícula Biliar</option>
+                                            <option value="">Cirugía de Bazo</option>
+                                            <option value="">Cirugía de Intestino Delgado</option>
+                                            <option value="">Cirugía de Intestino Grueso</option>
+                                            <option value="">Cirugía de Colon o Recto</option>
+                                        </optgroup>
+                                    </select>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <label for="tiempoCirugia">¿Hace cuánto tiempo se realizó la cirugía?</label>
+                                    <div class="input-group">
+                                        <input type="number" class="form-control form-control-sm" id="tiempoCirugia">
+                                        <select name="periodoCirugia" id="periodoCirugia" class="form-select form-select-sm">
+                                            <option value="Dias">Días</option>
+                                            <option value="Semanas">Semanas</option>
+                                            <option value="Meses">Meses</option>
+                                            <option value="Años">Años</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div class="row mt-3">
+                                <h5>Patologías</h5>
+                                <div class="col-md-6">
+
+                                </div>
+                            </div>
+
+                            <div class="row mt-3">
+                                <h5>Intolerancias</h5>
+                                <div class="col-md-6">
+
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
 
         </div>
     </div>
@@ -244,6 +402,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/icheck-bootstrap@3.0.1/icheck-bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/dist/css/bootstrap-select.min.css" rel="stylesheet">
+
+    <!-- Select2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
 
     <style>
         .card-body {
@@ -261,31 +424,66 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.18/js/bootstrap-select.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-    <script>
+    <!-- Select2 -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+   <script>
         function toggleCard(cardId) {
-        const card = document.getElementById(cardId);
-        const icon = card.previousElementSibling.querySelector("i.fa");
+            const card = document.getElementById(cardId);
+            const icon = card.previousElementSibling.querySelector("i.fa");
 
-        if (card.style.display === "none" || card.style.display === "") {
-            card.style.display = "block";
-            icon.classList.remove("fa-plus");
-            icon.classList.add("fa-minus");
-        } else {
-            card.style.display = "none";
-            icon.classList.remove("fa-minus");
-            icon.classList.add("fa-plus");
+            if (card.style.display === "none" || card.style.display === "") {
+                card.style.display = "block";
+                icon.classList.remove("fa-plus");
+                icon.classList.add("fa-minus");
+            } else {
+                card.style.display = "none";
+                icon.classList.remove("fa-minus");
+                icon.classList.add("fa-plus");
+            }
         }
-    }
 
-    // Abrir el primer card al cargar la página
-    document.addEventListener("DOMContentLoaded", function () {
-        const primerCard = document.getElementById("datosPersonales");
-        const iconPrimerCard = primerCard.previousElementSibling.querySelector("i.fa");
+        // Abrir el primer card al cargar la página
+        document.addEventListener("DOMContentLoaded", function () {
+            const primerCard = document.getElementById("datosPersonales");
+            const iconPrimerCard = primerCard.previousElementSibling.querySelector("i.fa");
 
-        primerCard.style.display = "block";
-        iconPrimerCard.classList.remove("fa-plus");
-        iconPrimerCard.classList.add("fa-minus");
-    });
+            primerCard.style.display = "block";
+            iconPrimerCard.classList.remove("fa-plus");
+            iconPrimerCard.classList.add("fa-minus");
+        });
+
+        //SELECT2
+        $( '#gustos' ).select2( {
+            theme: "bootstrap-5",
+            width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
+            placeholder: $( this ).data( 'placeholder' ),
+            closeOnSelect: false,
+        } );
+
+        $( '#no_gustos' ).select2( {
+            theme: "bootstrap-5",
+            width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
+            placeholder: $( this ).data( 'placeholder' ),
+            closeOnSelect: false,
+        } );
+
+        $( '#alergias' ).select2( {
+            theme: "bootstrap-5",
+            width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
+            placeholder: $( this ).data( 'placeholder' ),
+            closeOnSelect: false,
+        } );
+
+        $( '#cirugias' ).select2( {
+            theme: "bootstrap-5",
+            width: $( this ).data( 'width' ) ? $( this ).data( 'width' ) : $( this ).hasClass( 'w-100' ) ? '100%' : 'style',
+            placeholder: $( this ).data( 'placeholder' ),
+            closeOnSelect: false,
+        } );
+
     </script>
 
 @stop
