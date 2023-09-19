ad80e873e998c2f4bd4ddc70dade9c6f5adf2e7f
MarianoIvasiuta <luis.ivasiuta@fceqyn.unam.edu.ar>
2023-09-19 15:50:05 -0300

    Avancé en el método CREATE de alimentos. ME falta completar la validación de valores nutricionales.

--- app/Http/Controllers/admin/AlimentoController.php
+++ app/Http/Controllers/admin/AlimentoController.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Models\Alimento;
 use App\Models\FuenteAlimento;
 use App\Models\GrupoAlimento;
+use App\Models\Nutriente;
+use App\Models\TipoNutriente;
+use App\Models\ValorNutricional;
 use Illuminate\Http\Request;
 
 class AlimentoController extends Controller
@@ -19,7 +22,8 @@ class AlimentoController extends Controller
     {
         $alimentos = Alimento::all();
         $grupos = GrupoAlimento::all();
-        return view ('admin.gestion-alimentos.index', compact('alimentos', 'grupos'));
+        $nutrientes = Nutriente::all();
+        return view ('admin.gestion-alimentos.index', compact('alimentos', 'grupos', 'nutrientes'));
     }
 
     /**
@@ -31,7 +35,9 @@ class AlimentoController extends Controller
     {
         $grupos = GrupoAlimento::all();
         $fuentes = FuenteAlimento::all();
-        return view ('admin.gestion-alimentos.create', compact('grupos', 'fuentes'));
+        $nutrientes = Nutriente::all();
+        $tipo_nutrientes = TipoNutriente::all();
+        return view ('admin.gestion-alimentos.create', compact('grupos', 'fuentes', 'nutrientes', 'tipo_nutrientes'));
     }
 
     /**
@@ -42,9 +48,58 @@ class AlimentoController extends Controller
      */
     public function store(Request $request)
     {
+        //Validación y Creación del primer Form 'Datos de Alimento'
         $request->validate([
-            
+            'alimento' => ['required', 'string', 'max:30'],
+            'grupo_alimento' => ['required', 'integer', 'default:1'],
+            'estacional' => ['required', 'boolean', 'default:false'],
+            'estacion' => ['required', 'string', 'max:10', 'default:Ninguna'],
         ]);
+
+        $alimento = $request->input('alimento');
+        $grupo_alimento = $request->input('grupo_alimento');
+        $estacional = $request->input('estacional');
+        $estacion = $request->input('estacion');
+
+        //Validamos que no exista ese alimento
+        $alimento_existente = Alimento::where('alimento', $alimento)->first();
+
+        if ($alimento_existente) {
+            return redirect()->route('gestion-alimentos.create')->with('error', 'El alimento ya existe');
+        } else {
+
+            $alimentoNuevo = Alimento::create([
+                'alimento' => $alimento,
+                'grupo_alimento_id' => $grupo_alimento,
+                'estacional' => $estacional,
+                'estacion' => $estacion,
+            ]);
+
+            return redirect()->route('gestion-alimentos.create')->with('success', 'Alimento creado correctamente');
+
+            //Validación y creación del segundo Form 'Valores nutricionales'.
+            $request->validate([
+                'fuente' => ['required', 'integer', 'default:1'],
+                'nutriente' => ['required', 'integer'],
+                'unidad' => ['required', 'string', 'max:10'],
+                'valor' => ['required', 'numeric'],
+            ]);
+
+            $fuente = $request->input('fuente');
+            $nutriente = $request->input('nutriente');
+            $unidad = $request->input('unidad');
+            $valor = $request->input('valor');
+
+            ValorNutricional::create([
+                'alimento_id' => $alimentoNuevo->id,
+                'fuente_alimento_id' => $fuente,
+                'nutriente_id' => $nutriente,
+                'unidad' => $unidad,
+                'valor' => $valor,
+            ]);
+
+        }
+
     }
 
     /**
--- resources/views/admin/gestion-alimentos/create.blade.php
+++ resources/views/admin/gestion-alimentos/create.blade.php
@@ -92,27 +92,113 @@
                     </form>
                 </div>
 
-                    <div id="valor-nutricional" class="tab-pane">
-                        <form action="{{ route('gestion-alimentos.store') }}" method="POST">
-                            @csrf
-                            <div class="row">
-                                <label for="fuente">Fuente</label>
-                                <div class="col-10 mb-3">
-                                    <select class="form-select" name="fuente" id="fuente">
-                                        <option value="">Seleccione una opción...</option>
-                                        @foreach ($fuentes as $fuente)
-                                            <option value="{{ $fuente->id }}">{{ $fuente->fuente }}</option>
-                                        @endforeach
-                                    </select>
+                {{-- Form de Valores Nutricionales --}}
+                <div id="valor-nutricional" class="tab-pane">
+                    <form action="{{ route('gestion-alimentos.store') }}" method="POST">
+                        @csrf
+                        <div class="row">
+                            <label for="fuente">Fuente</label>
+                            <div class="col-10 mb-3">
+                                <select class="form-select" name="fuente" id="fuente">
+                                    <option value="">Seleccione una opción...</option>
+                                    @foreach ($fuentes as $fuente)
+                                        <option value="{{ $fuente->id }}">{{ $fuente->fuente }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-2 mb-3">
+                                <a href="{{ route('gestion-fuentes.create') }}" class="btn btn-primary">Nuevo</a>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12 mb-3">
+                                <label>Nutrientes</label>
+                            </div>
+
+                            <!-- Pestañas desplegables para Macronutrientes y Micronutrientes -->
+                            <div class="accordion" id="nutrientTabs">
+                                <!-- Macronutrientes -->
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="macronutrientHeading">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#macronutrientCollapse" aria-expanded="true" aria-controls="macronutrientCollapse">
+                                            Macronutrientes
+                                        </button>
+                                    </h2>
+                                    <div id="macronutrientCollapse" class="accordion-collapse collapse show" aria-labelledby="macronutrientHeading">
+                                        <div class="accordion-body">
+                                            <table class="table">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Nombre del Nutriente</th>
+                                                        <th>Unidad de Medida</th>
+                                                        <th>Valor</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @foreach ($nutrientes as $nutriente)
+                                                        @if ($nutriente->tipo_nutriente_id == 1)
+                                                            <tr>
+                                                                <td>{{ $nutriente->nombre_nutriente }}</td>
+                                                                <td>
+                                                                    <input type="text" name="nutriente_unidad_{{ $nutriente->id }}" class="form-control" placeholder="Unidad de Medida">
+                                                                </td>
+                                                                <td>
+                                                                    <input type="text" name="nutriente_valor_{{ $nutriente->id }}" class="form-control" placeholder="Valor">
+                                                                </td>
+                                                            </tr>
+                                                        @endif
+                                                    @endforeach
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="col-2 mb-3">
-                                    <a href="{{route('gestion-fuentes.create')}}" class="btn btn-primary">Nuevo</a>
+
+                                <!-- Micronutrientes -->
+                                <div class="accordion-item mb-3">
+                                    <h2 class="accordion-header" id="micronutrientHeading">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#micronutrientCollapse" aria-expanded="false" aria-controls="micronutrientCollapse">
+                                            Micronutrientes
+                                        </button>
+                                    </h2>
+                                    <div id="micronutrientCollapse" class="accordion-collapse collapse" aria-labelledby="micronutrientHeading">
+                                        <div class="accordion-body">
+                                            <table class="table">
+                                                <thead>
+                                                    <tr>
+                                                        <th>Nombre del Nutriente</th>
+                                                        <th>Unidad</th>
+                                                        <th>Valor</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    @foreach ($nutrientes as $nutriente)
+                                                        @if ($nutriente->tipo_nutriente_id == 2)
+                                                            <tr>
+                                                                <td>{{ $nutriente->nombre_nutriente }}</td>
+                                                                <td>
+                                                                    <input type="text" name="nutriente_unidad_{{ $nutriente->id }}" class="form-control" placeholder="Unidad">
+                                                                </td>
+                                                                <td>
+                                                                    <input type="text" name="nutriente_valor_{{ $nutriente->id }}" class="form-control" placeholder="Valor">
+                                                                </td>
+                                                            </tr>
+                                                        @endif
+                                                    @endforeach
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <a href="{{ route('gestion-alimentos.index') }}" class="btn btn-danger" tabindex="7">Cancelar</a>
-                            <button type="submit" class="btn btn-success">Guardar</button>
-                        </form>
-                    </div>
+                            <!-- Fin de pestañas desplegables -->
+                        </div>
+
+                        <a href="{{ route('gestion-alimentos.index') }}" class="btn btn-danger" tabindex="7">Cancelar</a>
+                        <button type="submit" class="btn btn-success">Guardar</button>
+                    </form>
+                </div>
 
 
             </div>
@@ -123,7 +209,6 @@
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- Select2 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
@@ -132,7 +217,6 @@
 
 @section('js')
     <script> console.log('Hi!'); </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- Select2 -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.slim.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -140,12 +224,14 @@
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 
-        <script>
+    <script>
         // Inicializa las pestañas al cargar la página
         $(document).ready(function () {
             $('.nav-tabs a').click(function () {
                 $(this).tab('show');
             });
         });
+
+
     </script>
 @stop
