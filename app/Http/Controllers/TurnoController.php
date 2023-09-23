ea347f468b50ea34cc9e6b4a445100070cfa0712
MarianoIvasiuta <luis.ivasiuta@fceqyn.unam.edu.ar>
2023-09-23 10:15:57 -0300

    Solicitud de turno completo.

--- app/Http/Controllers/TurnoController.php
+++ app/Http/Controllers/TurnoController.php
@@ -11,6 +11,7 @@ use App\Models\Paciente;
 use App\Models\Paciente\HistoriaClinica;
 use App\Models\TipoConsulta;
 use App\Models\Turno;
+use Carbon\Carbon;
 use DateInterval;
 use DateTime;
 use Illuminate\Http\Request;
@@ -25,7 +26,7 @@ class TurnoController extends Controller
     public function index()
     {
         $turnos = Turno::all();
-        $paciente = Paciente::where('id', auth()->user()->id)->first();
+        $paciente = Paciente::where('user_id', auth()->user()->id)->first();
         return view('paciente.turnos-paciente.index', compact('turnos', 'paciente'));
     }
 
@@ -85,7 +86,160 @@ class TurnoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //Validamos el formulario
+        $request->validate([
+            'profesional' => ['required', 'integer'],
+            'tipo_consulta' => ['required', 'integer'],
+            'fecha' => ['required', 'date'],
+            'hora' => ['required', 'date_format:H:i'],
+        ]);
+
+        //Obtenemos los datos del formulario
+        $profesional = $request->input('profesional');
+        $paciente = Paciente::where('user_id', auth()->user()->id)->first();
+        $tipo_consulta = $request->input('tipo_consulta');
+        $fecha = $request->input('fecha');
+        $hora = $request->input('hora');
+
+        //Buscamos el profesional seleccionado
+        $profesionalExistente = Nutricionista::find($profesional);
+
+        //Verificamos que exista
+        if(!$profesionalExistente){
+            return redirect()->back()->with('error', 'Profesional no válido');
+        }
+
+        //Buscamos el paciente
+        $pacienteExistente = Paciente::find($paciente->id);
+
+        //Verificamos que exista
+        if(!$pacienteExistente){
+            return redirect()->back()->with('error', 'Paciente no válido');
+        }
+
+        //Buscamos y verificamos que exista el tipo de consulta
+        $tipoConsultaExistente = TipoConsulta::find($tipo_consulta);
+
+        if(!$tipoConsultaExistente){
+            return redirect()->back()->with('error', 'Tipo de consulta no válido');
+        }
+
+        //Validamos que se solicite el turno con al menos 24 horas de anticipación
+        // Obtener la fecha y hora actual
+        $fechaActual = Carbon::now();
+        // Calcular la fecha mínima permitida (24 horas de anticipación)
+        $fechaMinima = $fechaActual->copy()->addDay();
+        //Obtenemos la fecha y hora seleccionada
+        $fechaElegida = Carbon::parse($fecha . ' ' . $hora);
+        //Comparamos las fechas
+        if($fechaElegida < $fechaMinima){
+            return redirect()->back()->with('error', 'Debes solicitar un turno con al menos 24 horas de anticipación.');
+        }
+
+        //Obtenemos la fecha actual con formato: año-mes-día y validamos
+        $fechaActual = date('Y-m-d');
+
+        if($fecha < $fechaActual){
+            return redirect()->back()->with('error', 'La fecha no puede ser anterior a la fecha actual');
+        }
+
+        $fechaActual = date('Y-m-d');
+
+        if($fecha == $fechaActual){
+            $horaActual = date('H:i');
+            if($hora < $horaActual){
+                return redirect()->back()->with('error', 'La hora no puede ser anterior a la hora actual');
+            }
+        }
+
+        $fechaActual = date('Y-m-d');
+
+        if($fecha == $fechaActual){
+            $horaActual = date('H:i');
+            if($hora == $horaActual){
+                return redirect()->back()->with('error', 'La hora no puede ser igual a la hora actual');
+            }
+        }
+
+        $fechaSeleccionada = $request->input('fecha');
+            $fechaNueva = new DateTime($fechaSeleccionada);
+            //Obtenemos el número del día de la semana
+            $numeroDiaSemana = $fechaNueva->format('w'); // 0: Domingo, 1: Lunes, 2: Martes, etc.
+            switch ($numeroDiaSemana) {
+                case 0:
+                    $diaSeleccionado = 'Domingo';
+                    break;
+                case 1:
+                    $diaSeleccionado = 'Lunes';
+                    break;
+                case 2:
+                    $diaSeleccionado = 'Martes';
+                    break;
+                case 3:
+                    $diaSeleccionado = 'Miercoles';
+                    break;
+                case 4:
+                    $diaSeleccionado = 'Jueves';
+                    break;
+                case 5:
+                    $diaSeleccionado = 'Viernes';
+                    break;
+                case 6:
+                    $diaSeleccionado = 'Sabado';
+                    break;
+                default:
+
+                    break;
+            }
+
+            $diasAtencion = DiasAtencion::where('dia', $diaSeleccionado)->get();
+            $horarios = HorariosAtencion::all();
+            $horasAtencion = HorasAtencion::all();
+
+            // Obtenemos la hora seleccionada en formato "H:i" (por ejemplo, "14:30")
+            $horaSeleccionada = $request->input('hora');
+
+            // Crea un objeto Carbon a partir de la hora seleccionada
+            $horaCarbon = Carbon::createFromFormat('H:i', $horaSeleccionada);
+
+            // Comprueba si la hora está en la mañana (antes de las 12:00 PM)
+            if ($horaCarbon->lt(Carbon::createFromTime(12, 0))) {
+                $periodoDelDia = 'Maniana';
+            } else {
+                $periodoDelDia = 'Tarde';
+            }
+
+            $horarioId = null;
+
+            foreach($diasAtencion as $diaAtencion){
+                foreach($horarios as $horario){
+                    if($diaAtencion->id == $horario->dia_atencion_id && $horario->nutricionista_id == $profesionalExistente->id){
+                        foreach($horasAtencion as $horaAtencion){
+                            if($horaAtencion->id == $horario->hora_atencion_id && $periodoDelDia == $horaAtencion->etiqueta){
+                                $horarioId = $horario->id;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+        $turnoCreado = Turno::create([
+            'paciente_id' => $pacienteExistente->id,
+            'horario_id' => $horarioId,
+            'tipo_consulta_id' => $tipo_consulta,
+            'fecha' => $fecha,
+            'hora' => $hora,
+            'estado' => 'Pendiente',
+        ]);
+
+        if(!$turnoCreado){
+            return redirect()->back()->with('error', 'Error al registrar el turno.');
+        }
+
+        return redirect()->route('turnos.index')->with('success', 'Turno registrado correctamente.');
+
+
     }
 
     /**
@@ -132,129 +286,6 @@ class TurnoController extends Controller
     {
         //
     }
-/*
-    public function horasDisponibles(Request $request){
-
-        $horarios = HorariosAtencion::all();
-        $dias = DiasAtencion::all();
-        $profesionalSeleccionado = $request->input('profesional');
-        $profesional = Nutricionista::find($profesionalSeleccionado);
-
-        if($profesional){
-            $fechaSeleccionada = $request->input('fecha');
-            $fecha = new DateTime($fechaSeleccionada);
-            //Obtenemos el número del día de la semana
-            $numeroDiaSemana = $fecha->format('w'); // 0: Domingo, 1: Lunes, 2: Martes, etc.
-            switch ($numeroDiaSemana) {
-                case 0:
-                    $diaSeleccionado = 'Domingo';
-                    break;
-                case 1:
-                    $diaSeleccionado = 'Lunes';
-                    break;
-                case 2:
-                    $diaSeleccionado = 'Martes';
-                    break;
-                case 3:
-                    $diaSeleccionado = 'Miercoles';
-                    break;
-                case 4:
-                    $diaSeleccionado = 'Jueves';
-                    break;
-                case 5:
-                    $diaSeleccionado = 'Viernes';
-                    break;
-                case 6:
-                    $diaSeleccionado = 'Sabado';
-                    break;
-                default:
-
-                    break;
-            }
-
-            $horasManiana = HorasAtencion::where('etiqueta', 'Maniana')->get();
-            $horasTarde = HorasAtencion::where('etiqueta', 'Tarde')->get();
-            $turnos = Turno::all();
-
-            $intervalo = new DateInterval('PT30M'); //Intervalo de 30 minutos
-            $horasDisponiblesManiana = [];
-            $horasDisponiblesTarde = [];
-
-            // Recorremos las horas de la mañana para dividirlas en un rango de 30 minutos
-            foreach ($horasManiana as $horaManiana) {
-                foreach($dias as $dia){
-                    foreach($horarios as $horario){
-                        if($dia->id == $horario->dia_atencion_id && $horario->nutricionista_id == $profesional->id && $diaSeleccionado == $dia->dia && $horaManiana->id == $horario->hora_atencion_id){
-                            $horaInicio = new DateTime($horaManiana->hora_inicio);
-                            $horaFin = new DateTime($horaManiana->hora_fin);
-                            while ($horaInicio < $horaFin) {
-                                $horaActual = $horaInicio->format('H:i');
-                                $horaOcupada = false; // Variable para indicar si la hora está ocupada
-
-                                // Comprobamos si hay un turno pendiente en esta hora
-                                foreach ($turnos as $turno) {
-                                    if ($turno->fecha == $fechaSeleccionada && $turno->hora == $horaActual && $turno->estado == 'Pendiente') {
-                                        $horaOcupada = true;
-                                        break; // No necesitamos seguir buscando
-                                    }
-                                }
-
-                                // Si la hora no está ocupada, la agregamos a las disponibles
-                                if (!$horaOcupada) {
-                                    $horasDisponiblesManiana[] = $horaActual;
-                                }
-                            }
-                        }
-                    }
-                    // Incrementamos la hora actual
-                    $horaInicio->add($intervalo);
-                }
-            }
-
-            //Recorremos las horas de la tarde para dividirla en un rango de 30 minutos
-
-            foreach ($horasTarde as $horaTarde) {
-                foreach($dias as $dia){
-                    foreach($horarios as $horario){
-                        if($dia->id == $horario->dia_atencion_id && $horario->nutricionista_id == $profesional->id && $diaSeleccionado == $dia->dia && $dia->seleccionado == 1 && $horaTarde->id == $horario->hora_atencion_id){
-                            $horaInicio = new DateTime($horaTarde->hora_inicio);
-                            $horaFin = new DateTime($horaTarde->hora_fin);
-                            while ($horaInicio < $horaFin) {
-                                $horaActual = $horaInicio->format('H:i');
-                                $horaOcupada = false; // Variable para indicar si la hora está ocupada
-
-                                // Comprobamos si hay un turno pendiente en esta hora
-                                foreach ($turnos as $turno) {
-                                    if ($turno->fecha == $fechaSeleccionada && $turno->hora == $horaActual && $turno->estado == 'Pendiente') {
-                                        $horaOcupada = true;
-                                        break; // No necesitamos seguir buscando
-                                    }
-                                }
-
-                                // Si la hora no está ocupada, la agregamos a las disponibles
-                                if (!$horaOcupada) {
-                                    $horasDisponiblesTarde[] = $horaActual;
-                                }
-
-                                // Incrementamos la hora actual
-                                $horaInicio->add($intervalo);
-                            }
-                        }
-                    }
-                }
-            }
-            return response()->json([
-                'horasDisponiblesManiana' => $horasDisponiblesManiana,
-                'horasDisponiblesTarde' => $horasDisponiblesTarde
-            ]);
-        }else{
-            return response()->json(['error' => 'Profesional no válido']);
-        }
-
-    }
-*/
-
-//Crea la función horasDisponibles(Request $request) y que las horas que se muestren sean del profesional seleccionado, y que se muestren las horas libres solo si la fecha seleccionada coincide con los días de atención de dicho profesional
 
     public function horasDisponibles(Request $request){
 
--- resources/views/paciente/turnos-paciente/create.blade.php
+++ resources/views/paciente/turnos-paciente/create.blade.php
@@ -8,115 +8,124 @@
 
 @section('content')
 
-    <div class="card card-dark">
-        <div class="card-header">
-            <h5>Solicitud de turno</h5>
+        @if(auth()->user()->tipo_usuario === 'Paciente' && !app('App\Http\Controllers\PacienteController')->hasCompletedHistory())
+
+        <div class="alert alert-warning" role="alert">
+            <h5>Historia Clínica icompleta</h5>
+            Parece que aún no has completado tu Historia Clínica. <br>
+            Para tener acceso a esta funcionalidad del sistema, necesita completar su historia clínica. <br>
+            Haga click en el siguiente enlace para completar su historia clínica:
+            <br><a href="{{ route('historia-clinica.create') }}" class="alert-link">Completar mi Historia Clínica</a>
         </div>
 
-        <div class="card-body">
-            <form action="{{route('turnos.store')}}" method="POST">
-                @csrf
-
-                <div class="row">
-                    <div class="col-md-6">
-                        <label class="form-label" for="profesional">Profesional</label>
-                        <select name="profesional" id="profesional" class="form-select">
-                            <option value="">Seleccione un profesional</option>
-                            @foreach($profesionales as $profesional)
-                                <option value="{{$profesional->id}}">{{$profesional->user->name}} {{$profesional->user->apellido}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-
-                    <div class="col-md-6">
-                        <label class="form-label" for="paciente">Paciente</label>
-                        <input
-                            @foreach ($pacientes as $paciente)
-                                @if ($paciente->user_id == auth()->user()->id)
-                                value="{{$paciente->user->name}} {{$paciente->user->apellido}}"
-                                @endif
-                            @endforeach
-                        class="form-control" name="paciente" id="paciente" type="text" disabled>
-                    </div>
+        @else
+            <div class="card card-dark">
+                <div class="card-header">
+                    <h5>Solicitud de turno</h5>
                 </div>
 
-                <div class="row mt-3">
-                    <div class="col-md-6">
-                        <label class="form-label" for="tipo_consulta">Tipo de consulta</label>
-                        <select class="form-select" name="tipo_consulta" id="tipo_consulta">
-                            <option value="">Seleccione un tipo de consulta</option>
-                            @foreach($tipo_consultas as $tipo_consulta)
-                                <option value="{{$tipo_consulta->id}}">{{$tipo_consulta->tipo_consulta}}</option>
-                            @endforeach
-                        </select>
-                    </div>
+                <div class="card-body">
+                    <form action="{{route('turnos.store')}}" method="POST">
+                        @csrf
 
-                    <div class="col-md-6">
-                        <label class="form-label" for="objetivo_salud">Objetivo de salud</label>
-                        <select class="form-select" name="objetivo_salud" id="objetivo_salud" disabled>
-                            @foreach ($pacientes as $paciente)
-                                @foreach ($historias_clinicas as $historia_clinica)
-                                    @if ($paciente->user_id == auth()->user()->id)
-                                        @if ($historia_clinica->paciente_id == $paciente->id)
-                                            <option value="{{$historia_clinica->objetivo_salud}}">{{$historia_clinica->objetivo_salud}}</option>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <label class="form-label" for="profesional">Profesional</label>
+                                <select name="profesional" id="profesional" class="form-select">
+                                    <option value="">Seleccione un profesional</option>
+                                    @foreach($profesionales as $profesional)
+                                        <option value="{{$profesional->id}}">{{$profesional->user->name}} {{$profesional->user->apellido}}</option>
+                                    @endforeach
+                                </select>
+
+                                @error('profesional')
+                                    <small class="text-danger">{{$message}}</small>
+                                @enderror
+                            </div>
+
+                            <div class="col-md-6">
+                                <label class="form-label" for="paciente">Paciente</label>
+                                <input
+                                    @foreach ($pacientes as $paciente)
+                                        @if ($paciente->user_id == auth()->user()->id)
+                                        value="{{$paciente->user->name}} {{$paciente->user->apellido}}"
                                         @endif
-                                    @endif
-                                @endforeach
-                            @endforeach
-                        </select>
-                    </div>
-                </div>
+                                    @endforeach
+                                class="form-control" name="paciente" id="paciente" type="text" disabled>
 
-                <div class="row mt-3" id="horarios-disponibles">
-                    <div class="col-md-12" id="fecha-turno">
-                        <label class="form-label" for="fecha">Fecha</label>
-                        <input class="form-control" type="date" name="fecha" id="fecha">
-                    </div>
-{{--
-                    <div class="col-md-12" id="hora-disponible">
-                        @foreach ($horarios as $horario)
-                            @foreach($profesionales as $profesional)
-                                @if ($horario->nutricionista_id == $profesional->id)
-                                    @foreach ($horas as $hora)
-                                        @foreach ($dias as $dia)
-                                            @if ($horario->hora_id == $hora->id && $horario->dia_id == $dia->id)
-                                                @foreach ($horasDisponiblesManiana as $horaDisponibleManiana)
-                                                    @foreach ($horasDisponiblesTarde as $horaDisponibleTarde)
-                                                        @foreach ($turnos as $turno)
-                                                            @if ($turno->hora == $horaDisponibleManiana && $turno->estado == 'Pendiente')
-                                                                <span class="hora-disponible-maniana">{{ $horaDisponibleManiana }}</span>
-                                                                <span class="hora-disponible-tarde">{{ $horaDisponibleTarde }}</span>
-                                                            @endif
-                                                        @endforeach
-                                                    @endforeach
-                                                @endforeach
+                                @error('paciente')
+                                    <small class="text-danger">{{$message}}</small>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mt-3">
+                            <div class="col-md-6">
+                                <label class="form-label" for="tipo_consulta">Tipo de consulta</label>
+                                <select class="form-select" name="tipo_consulta" id="tipo_consulta">
+                                    <option value="">Seleccione un tipo de consulta</option>
+                                    @foreach($tipo_consultas as $tipo_consulta)
+                                        <option value="{{$tipo_consulta->id}}">{{$tipo_consulta->tipo_consulta}}</option>
+                                    @endforeach
+                                </select>
+                                @error('tipo_consulta')
+                                    <small class="text-danger">{{$message}}</small>
+                                @enderror
+                            </div>
+
+                            <div class="col-md-6">
+                                <label class="form-label" for="objetivo_salud">Objetivo de salud</label>
+                                <select class="form-select" name="objetivo_salud" id="objetivo_salud" disabled>
+                                    @foreach ($pacientes as $paciente)
+                                        @foreach ($historias_clinicas as $historia_clinica)
+                                            @if ($paciente->user_id == auth()->user()->id)
+                                                @if ($historia_clinica->paciente_id == $paciente->id)
+                                                    <option value="{{$historia_clinica->objetivo_salud}}">{{$historia_clinica->objetivo_salud}}</option>
+                                                @endif
                                             @endif
                                         @endforeach
                                     @endforeach
-                                @endif
-                            @endforeach
-                        @endforeach
-                    </div>--}}
-                </div>
-
-                <div class="row mt-3">
-                    <div class="col-md-12">
-                        <label for="hora">Horas Disponibles</label>
-                        <br>
-                        <div class="btn-group" data-toggle="buttons" id="horas-disponibles">
-                            <!-- Las horas disponibles se agregarán aquí dinámicamente -->
+                                </select>
+                                @error('objetivo_salud')
+                                    <small class="text-danger">{{$message}}</small>
+                                @enderror
+                            </div>
                         </div>
-                        <div id="mensaje-error" class="alert alert-danger" style="display: none;">
-                            No hay horarios disponibles para la fecha seleccionada o este día no se realizan consultas.
+
+                        <div class="row mt-3" id="horarios-disponibles">
+                            <div class="col-md-12" id="fecha-turno">
+                                <label class="form-label" for="fecha">Fecha</label>
+                                <input class="form-control" type="date" name="fecha" id="fecha">
+
+                                @error('fecha')
+                                    <small class="text-danger">{{$message}}</small>
+                                @enderror
+                            </div>
+
                         </div>
-                    </div>
+
+                        <div class="row mt-3">
+                            <div class="col-md-12">
+                                <label for="hora">Horas Disponibles</label>
+                                <br>
+                                <div class="btn-group" data-toggle="buttons" id="horas-disponibles">
+                                    <!-- Las horas disponibles se agregarán aquí dinámicamente -->
+                                    @error('hora')
+                                        <small class="text-danger">{{$message}}</small>
+                                    @enderror
+                                </div>
+                                <div id="mensaje-error" class="alert alert-danger" style="display: none;">
+                                    No hay horarios disponibles para la fecha seleccionada o este día no se realizan consultas.
+                                </div>
+                            </div>
+                        </div>
+
+                        <button class="btn btn-success mt-3" type="submit">Solicitar turno</button>
+
+                    </form>
                 </div>
-
-                <button class="btn btn-success mt-3" type="submit">Solicitar turno</button>
-
-            </form>
-        </div>
-    </div>
+            </div>
+        @endif
 
 @stop
 
