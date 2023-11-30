8c80a37e3cb2689b4ca83135b225dade7b4fdf09
MarianoIvasiuta <luis.ivasiuta@fceqyn.unam.edu.ar>
2023-11-30 09:38:38 -0300

    instalé el paquete laravel auditing y configuré el mismo para la auditoria

--- app/Models/Alimento.php
+++ app/Models/Alimento.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class Alimento extends Model
+class Alimento extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
     use HasFactory;
 
     protected $fillable = [
